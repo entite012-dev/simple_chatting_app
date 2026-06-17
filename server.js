163a448c9761105e95b9a4dd31102ca923a544ca
entite012-dev <entite012@gmail.com>
2026-06-18 00:18:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-const db = require('./databases');
+const db = require('./database');
 
 const app = express();
 const server = http.createServer(app);
